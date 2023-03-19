241a010ec8f74fb2b3e8e6c8ed1e842ddf6f47a2
grok1 <95889997+grok1@users.noreply.github.com>
2023-03-19 17:20:49 +0300

    refactor code

--- code_review/dataset_utils.py
+++ code_review/dataset_utils.py
@@ -1,21 +1,15 @@
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import f1_score
 from pymorphy2 import MorphAnalyzer
 from collections import namedtuple
 from nltk.corpus import stopwords
 from multiprocessing import Pool
-import matplotlib.pyplot as plt
 from tqdm import tqdm
-import seaborn as sns
 import pandas as pd
-import numpy as np
 import warnings
 import glob
 import re
 import os
 
-plt.style.use('ggplot')
+
 morph = MorphAnalyzer()
 
 
@@ -98,43 +92,6 @@ def df_from_txt_files(dataset_name, dir_path="./texts"):
         })
     df_raw = pd.DataFrame(dataset_list)
     return df_raw
-
-
-def show_work_titles_histplot(df, title):
-    sns.set(rc={'figure.figsize': (14, 4)})
-    y = sns.histplot(data=df, x='author_surname')
-    y.set(xlabel='', ylabel='Количество произведений')
-    y.bar_label(y.containers[0])
-    plt.title(title)
-    max_num = df.author.value_counts().max()
-    plt.yticks(np.arange(0, max_num+1, 5))
-    plt.show()
-
-
-def show_excerpts_histplot(df, title, num_of_words=None):
-    sns.set(rc={'figure.figsize': (14, 4)})
-    y = sns.histplot(data=df, x='author_surname')
-    if num_of_words is None:
-        sample_size = min(df.shape[0], 500)
-        n_words_mean = df.sample(sample_size).text\
-            .apply(lambda x: len(x.split())).mean()
-        num_of_words = int(round(n_words_mean, -1))
-    n_words_text = fr' по $\approx {num_of_words}$ слов'
-    y.set(xlabel='', ylabel='Количество отрывков' + n_words_text)
-    y.bar_label(y.containers[0])
-    plt.title(title)
-    plt.show()
-
-
-def show_lemmas_histplot(df, title):
-    sns.set(rc={'figure.figsize': (14,4)})
-    hst = sns.histplot(
-        data=df['lemmas'].apply(lambda x: len(x.split())),
-    )
-    hst.set(xlabel='Количество лемм в одном отрывке')
-    hst.set(ylabel='Число отрывков')
-    plt.title(title)
-    plt.show()
 
 
 def sentence_generator(text, offset=0, punct_marks=".?!…", yield_pos=False):
@@ -303,45 +260,6 @@ def add_lemmas_column(df, inplace=False, verbose=1, n_jobs=6):
     return df
 
 
-def author_rus_surnames(df: pd.DataFrame) -> dict:
-    """Возвращает словарь 'значение из столбца author (уник. для каждого автора)' -
-    'фамилия автора на русском (из столбца author_surname)'.
-    Возвращаемый словарь можно передать функции `plot_confusion_matrix`
-    аргументом `rus_translation`.
-    """
-    return df.groupby('author')['author_surname'].describe().top.to_dict()
-
-
-def plot_confusion_matrix(y_true: pd.Series, y_pred: pd.Series, rus_translation=None):
-    """Выводит матрицу ошибок в читаемом виде.
-    """
-    labels = y_true.unique()
-    if rus_translation is not None:
-        labels_rus = list(map(lambda x: rus_translation[x], labels))
-    else:
-        labels_rus = labels
-    sns.set(rc={'figure.figsize': (5, 4)})
-    sns.heatmap(
-        confusion_matrix(y_true, y_pred, labels=labels),
-        xticklabels=labels_rus,
-        yticklabels=labels_rus,
-        annot=True,
-        fmt="d"
-    )
-    plt.show()
-
-
-def false_predictions(df, y_pred) -> pd.DataFrame:
-    """Возвращает DataFrame из объектов, для которых были сделаны
-    ошибочные предсказания. Первые два столбца - ответы и предсказания.
-    """
-    y_true = df.author
-    res = pd.DataFrame(np.vstack([y_true, y_pred]).T, columns=['y_true', 'y_pred'])
-    res = pd.concat([res, df], axis=1)
-    res = res.drop('author', axis=1)
-    return res[res.y_true != res.y_pred]
-
-
 def undersampling(df):
     res_df = df.copy(deep=True)
     min_count = df.author.value_counts().min()
@@ -360,4 +278,3 @@ def df_leave_authors(df, authors):
         else:
             mask = mask | (df.author == author)
     return df[mask].copy(deep=True)
-
