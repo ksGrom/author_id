b2ae4a5a5911422b9724a4916b87d0e89667e7e3
grok1 <95889997+grok1@users.noreply.github.com>
2023-04-05 02:26:20 +0300

    rename files

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,36 +1,41 @@
-from argparse import ArgumentParser
+from fastapi import FastAPI, HTTPException
+from fastapi import File, UploadFile
+from fastapi.responses import StreamingResponse
+from io import BytesIO, StringIO
+from pydantic import BaseModel
+from typing import List, Union
+from time import time
 import joblib
-import nltk
+
 from dataset_utils import (
-    input_file_to_df, 
-    add_lemmas_column
+    input_file_to_df,
 )
 
-nltk.download('stopwords')
+app = FastAPI()
 
 
-def parse_args():
-    parser = ArgumentParser('Text classification')
-    parser.add_argument(
-        '--input', type=str, required=True,
-        help='Path to txt file with one text or csv file with several texts.'
-    )
-    parser.add_argument(
-        '--output', type=str, required=True,
-        help='Path to output csv file with predictions.'
-    )
-    return parser.parse_args()
+class UploadedTextsResults(BaseModel):
+    n_texts: int
+    predictions: List[Union[int, str]]
+    exec_time: float
 
 
-def main(args):
-    with open(args.input, 'r') as f:
-        df = input_file_to_df(f)
-    df = add_lemmas_column(df)
+@app.get('/')
+async def root():
+    return {'message': 'OK'}
+
+
+@app.post("/predict_items")
+def predict_items(upload_file: UploadFile = File(...)) -> UploadedTextsResults:
+    start_t = time()
+    try:
+        df = input_file_to_df(upload_file.file, upload_file.filename)
+    except ValueError as e:
+        raise HTTPException(status_code=422, detail=f"Invalid file: {e}")
     clf = joblib.load("default.pkl")
-    df['predictions'] = clf.predict(df)
-    df.to_csv(args.output)
-
-
-if __name__ == '__main__':
-    args = parse_args()
-    main(args)
+    predictions = clf.predict(df).tolist()
+    return UploadedTextsResults(
+        n_texts=len(predictions),
+        predictions=predictions,
+        exec_time=time()-start_t
+    )
